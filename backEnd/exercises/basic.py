09edea1a763eed4b37bc3f22a95267fb85953f44
Mir Mirani <mir.mirani@outlook.com>
2020-02-01 13:17:29 +0000

    all

---
+++ backEnd/exercises/basic.py
@@ -0,0 +1,29 @@
+#basic exercises
+#1 notes
+print(''' Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are ''')
+
+#2 version print
+import sys
+print('Python version')
+print(sys.version)
+print('version info')
+print(sys.version_info)
+
+#3 current date/time
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+#4 area of circle
+import  math
+try 'what is the radius?':
+x = math.pi*r
+print()
+
+#continue from https://www.w3resource.com/python-exercises/python-basic-exercises.php
---
+++ frontEnd/htmlcss/Rough/jsnotes.js
@@ -0,0 +1,8 @@
+// n1
+    function favoriteAnimal(animal) {
+      return animal + " is my favorite animal!" + '<hr>';
+    }
+    
+    document.getElementById('n2').innerHTML= favoriteAnimal('Goat');
+//n2
+    
